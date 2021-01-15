f1c84bcff4a808f03783b6e4a672a60b72043e36
Oyekunle Oloyede <oyekunlemac@gmail.com>
2021-01-15 15:35:35 +0100

    Created the silent tracer

--- trace/tracer.go
+++ trace/tracer.go
@@ -15,13 +15,23 @@ type tracer struct {
 	out io.Writer
 }
 
+type nilTracer struct{}
+
 func (t *tracer) Trace(a ...interface{}) {
 	fmt.Fprint(t.out, a...)
 	fmt.Fprintln(t.out)
 }
 
+// New creates a new Tracer and returns a reference to it.
 func New(w io.Writer) Tracer {
 	return &tracer{
 		out: w,
 	}
+}
+
+func (t *nilTracer) Trace(a ...interface{}) {}
+
+// Off creates a Tracer that will ignore calls to Trace.
+func Off() Tracer {
+	return &nilTracer{}
 }
