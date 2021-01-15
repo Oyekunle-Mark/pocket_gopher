83fec4b48039553be15e12585b6569232b24e3f4
Oyekunle Oloyede <oyekunlemac@gmail.com>
2021-01-15 14:48:28 +0100

    turned room into an http handler

--- chat/room.go
+++ chat/room.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"github.com/gorilla/websocket"
+	"log"
+	"net/http"
+)
+
 type room struct {
 	forward chan []byte
 	join    chan *client
@@ -21,4 +27,37 @@ func (r *room) run() {
 			}
 		}
 	}
+}
+
+const (
+	socketBufferSize  = 1024
+	messageBufferSize = 256
+)
+
+var upgrader = &websocket.Upgrader{
+	ReadBufferSize:  socketBufferSize,
+	WriteBufferSize: socketBufferSize,
+}
+
+func (r *room) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	socker, err := upgrader.Upgrade(w, req, nil)
+
+	if err != nil {
+		log.Fatal("ServeHTTP: ", err)
+		return
+	}
+
+	client := &client{
+		socket: socker,
+		send:   make(chan []byte, messageBufferSize),
+		room:   r,
+	}
+
+	r.join <- client
+	defer func() {
+		r.leave <- client
+	}()
+
+	go client.write()
+	client.read()
 }
