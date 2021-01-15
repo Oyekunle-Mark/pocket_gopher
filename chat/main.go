0c4c1a858d4bc31f4f50176c0e57bdc31dbe5dc1
Oyekunle Oloyede <oyekunlemac@gmail.com>
2021-01-15 14:13:53 +0100

    template handler now an HTTP handler

--- chat/main.go
+++ chat/main.go
@@ -4,6 +4,7 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"path/filepath"
 	"sync"
 )
 
@@ -11,6 +12,15 @@ type templateHandler struct {
 	once     sync.Once
 	filename string
 	templ    *template.Template
+}
+
+// ServeHTTP handles the HTTP request.
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+	})
+
+	t.templ.Execute(w, nil)
 }
 
 func main() {
