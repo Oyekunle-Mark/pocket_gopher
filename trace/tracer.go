174350831ecc5305d665db09a1a71107037bc8c0
Oyekunle Oloyede <oyekunlemac@gmail.com>
2021-01-15 15:10:12 +0100

    tracer package

--- trace/tracer.go
+++ trace/tracer.go
@@ -1,6 +1,7 @@
 package trace
 
-// Tracer is the interface that describes an object capable of // tracing events throughout code.
+// Tracer is the interface that describes an object capable of
+// tracing events throughout code.
 type Tracer interface {
 	Trace(...interface{})
 }
