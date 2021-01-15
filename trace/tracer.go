d43dad20d9e71be21fe8ad968fea2907a0a6a4c8
Oyekunle Oloyede <oyekunlemac@gmail.com>
2021-01-15 15:16:17 +0100

    New returns the reference to a new Tracer type

--- trace/tracer.go
+++ trace/tracer.go
@@ -1,7 +1,22 @@
 package trace
 
+import "io"
+
 // Tracer is the interface that describes an object capable of
 // tracing events throughout code.
 type Tracer interface {
 	Trace(...interface{})
+}
+
+type tracer struct {
+	out io.Writer
+}
+
+func (t *tracer) Trace(a ...interface{}) {
+}
+
+func New(w io.Writer) Tracer {
+	return &tracer{
+		out: w,
+	}
 }
