015a3af227aa48f570a8eac39229cce7130edfe1
Oyekunle Oloyede <oyekunlemac@gmail.com>
2021-01-18 15:20:06 +0100

    updated clients Read method

--- chat/client.go
+++ chat/client.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/gorilla/websocket"
+	"time"
 )
 
 type client struct {
@@ -14,11 +15,16 @@ func (c *client) read() {
 	defer c.socket.Close()
 
 	for {
-		_, msg, err := c.socket.ReadMessage()
+		var msg *message
+
+		err := c.socket.ReadJSON(&msg)
 
 		if err != nil {
 			return
 		}
+
+		msg.When = time.Now()
+		msg.Name = c.userData["name"].(string)
 
 		c.room.forward <- msg
 	}
