11d8a8351e3f1d7e45410d93aab0ae9227b483e3
Oyekunle Oloyede <oyekunlemac@gmail.com>
2021-02-03 20:38:26 +0100

    json types

---
+++ thesaurus/bighuge.go
@@ -0,0 +1,15 @@
+package thesaurus
+
+type BigHuge struct {
+	APIKey string
+}
+
+type synonyms struct {
+	Noun *words `json:"noun"`
+	Verb *words `json:"verb"`
+}
+
+type words struct {
+	Syn []string `json:"syn"`
+}
+
