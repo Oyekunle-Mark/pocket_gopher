e477b49b54ddaaa4d33769805c222b9b32794409
Oyekunle Oloyede <oyekunlemac@gmail.com>
2021-02-06 10:43:22 +0100

    Dials and closes a mongo session

--- socialpoll/twittervotes/main.go
+++ socialpoll/twittervotes/main.go
@@ -1,3 +1,24 @@
 package main
 
+import (
+	"gopkg.in/mgo.v2"
+	"log"
+)
+
+var db *mgo.Session
+
 func main() {}
+
+func dialDb() error {
+	var err error
+	log.Println("dialing mongodb: localhost")
+
+	db, err = mgo.Dial("localhost")
+
+	return err
+}
+
+func closeDb() {
+	db.Close()
+	log.Println("closed database connection")
+}
