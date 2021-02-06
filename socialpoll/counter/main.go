bb095abeb89f1bab911c018616f2dd8323c34d7f
Oyekunle Oloyede <oyekunlemac@gmail.com>
2021-02-06 12:32:28 +0100

    handle votes publication

--- socialpoll/counter/main.go
+++ socialpoll/counter/main.go
@@ -3,12 +3,16 @@ package main
 import (
 	"flag"
 	"fmt"
+	"github.com/bitly/go-nsq"
 	"gopkg.in/mgo.v2"
 	"log"
 	"os"
+	"sync"
 )
 
 var fatalError error
+var counts map[string]int
+var countsLock sync.Mutex
 
 func fatal(e error) {
 	fmt.Println(e)
@@ -37,4 +41,26 @@ func main() {
 	}()
 
 	pollData := db.DB("ballots").C("polls")
+
+	log.Println("Connecting to nsq...")
+	q, err := nsq.NewConsumer("votes", "counter", nsq.NewConfig())
+
+	if err != nil {
+		fatal(err)
+		return
+	}
+
+	q.AddHandler(nsq.HandlerFunc(func(m *nsq.Message) error {
+		countsLock.Lock()
+		defer countsLock.Unlock()
+
+		if counts == nil {
+			counts = make(map[string]int)
+		}
+
+		vote := string(m.Body)
+		counts[vote]++
+
+		return nil
+	}))
 }
