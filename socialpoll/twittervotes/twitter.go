b955ac7d35970f6c8f1a3dd1989c38d0f8cd8ab8
Oyekunle Oloyede <oyekunlemac@gmail.com>
2021-02-06 10:29:41 +0100

    Read environment variables

--- socialpoll/twittervotes/twitter.go
+++ socialpoll/twittervotes/twitter.go
@@ -2,12 +2,20 @@ package main
 
 import (
 	"io"
+	"log"
 	"net"
 	"time"
+
+	"github.com/joeshaw/envdecode"
+	"github.com/matryer/go-oauth/oauth"
 )
 
 var conn net.Conn
 var reader io.ReadCloser
+var (
+	authClient *oauth.Client
+	creds      *oauth.Credentials
+)
 
 func dial(network, address string) (net.Conn, error) {
 	if conn != nil {
@@ -31,5 +39,30 @@ func closeConnection() {
 
 	if reader != nil {
 		reader.Close()
+	}
+}
+
+func setupTwitterAuth() {
+	var ts struct {
+		ConsumerKey    string `env:"SP_TWITTER_KEY,required"`
+		ConsumerSecret string `env:"SP_TWITTER_SECRET,required"`
+		AccessToken    string `env:"SP_TWITTER_ACCESSTOKEN,required"`
+		AccessSecret   string `env:"SP_TWITTER_ACCESSSECRET,required"`
+	}
+
+	if err := envdecode.Decode(&ts); err != nil {
+		log.Fatalln(err)
+	}
+
+	creds = &oauth.Credentials{
+		Token:  ts.AccessToken,
+		Secret: ts.AccessSecret,
+	}
+
+	authClient = &oauth.Client{
+		Credentials: oauth.Credentials{
+			Token:  ts.ConsumerKey,
+			Secret: ts.ConsumerSecret,
+		},
 	}
 }
