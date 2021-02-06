a9e198fdaf95b4a303a70907de6a78c564500eb4
Oyekunle Oloyede <oyekunlemac@gmail.com>
2021-02-06 11:13:13 +0100

    Reads text from twitter and find if option is there

--- socialpoll/twittervotes/twitter.go
+++ socialpoll/twittervotes/twitter.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"encoding/json"
 	"io"
 	"log"
 	"net"
 	"net/http"
 	"net/url"
 	"strconv"
+	"strings"
 	"sync"
 	"time"
 
@@ -93,4 +95,61 @@ func makeRequest(req *http.Request, params url.Values) (*http.Response, error) {
 		req.URL, params))
 
 	return httpClient.Do(req)
+}
+
+type tweet struct {
+	Text string
+}
+
+func readFromTwitter(votes chan<- string) {
+	options, err := loadOption()
+
+	if err != nil {
+		log.Println("failed to load options:", err)
+		return
+	}
+
+	u, err := url.Parse("https://stream.twitter.com/1.1/statuses/filter.json")
+
+	if err != nil {
+		log.Println("creating filter request failed:", err)
+		return
+	}
+
+	query := make(url.Values)
+	query.Set("track", strings.Join(options, ","))
+
+	req, err := http.NewRequest("POST", u.String(), strings.NewReader(query.Encode()))
+
+	if err != nil {
+		log.Println("creating filter request failed:", err)
+		return
+	}
+
+	response, err := makeRequest(req, query)
+
+	if err != nil {
+		log.Println("making request failed:", err)
+		return
+	}
+
+	reader := response.Body
+	decoder := json.NewDecoder(reader)
+
+	for {
+		var t tweet
+
+		if err := decoder.Decode(&t); err != nil {
+			break
+		}
+
+		for _, option := range options {
+			if strings.Contains(
+				strings.ToLower(t.Text),
+				strings.ToLower(option)) {
+				log.Println("vote:", option)
+				votes <- option
+			}
+		}
+	}
 }
