5390fdb551d06c0f88eedd5a937f7dd7bcba1f8a
Oyekunle Oloyede <oyekunlemac@gmail.com>
2021-02-07 18:06:31 +0100

    api key context and helper function

---
+++ api/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "context"
+
+func main() {}
+
+type contextKey struct {
+	name string
+}
+
+var contextKeyAPIKey = &contextKey{"api-key"}
+
+func APIKey(ctx context.Context) (string, bool) {
+	key, ok := ctx.Value(contextKeyAPIKey).(string)
+	return key, ok
+}
