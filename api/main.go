2d843649c9525576793abdaa667805812e975d71
Oyekunle Oloyede <oyekunlemac@gmail.com>
2021-02-07 18:11:01 +0100

    withAPIKey handlerFunc wrapper

--- api/main.go
+++ api/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "context"
+import (
+	"context"
+	"net/http"
+)
 
 func main() {}
 
@@ -13,4 +16,18 @@ var contextKeyAPIKey = &contextKey{"api-key"}
 func APIKey(ctx context.Context) (string, bool) {
 	key, ok := ctx.Value(contextKeyAPIKey).(string)
 	return key, ok
+}
+
+func withAPIKey(fn http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		key := r.URL.Query().Get("key")
+
+		if !isValidAPIKey(key) {
+			respondErr(w, r, http.StatusUnauthorized, "invalid API key")
+			return
+		}
+
+		ctx := context.WithValue(r.Context(), contextKeyAPIKey, key)
+		fn(w, r.WithContext(ctx))
+	}
 }
