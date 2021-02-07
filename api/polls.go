c38a891c430d4744ae2a0b9f84946b89e7fdaac3
Oyekunle Oloyede <oyekunlemac@gmail.com>
2021-02-07 19:04:25 +0100

    Placeholder for reading pools

--- api/polls.go
+++ api/polls.go
@@ -1,6 +1,10 @@
 package main
 
-import "gopkg.in/mgo.v2/bson"
+import (
+	"errors"
+	"gopkg.in/mgo.v2/bson"
+	"net/http"
+)
 
 type poll struct {
 	ID      bson.ObjectId  `bson:"_id" json:"id"`
@@ -8,4 +12,32 @@ type poll struct {
 	Options []string       `json:"options"`
 	Results map[string]int `json:"results,omitempty"`
 	APIKey  string         `json:"apiKey"`
+}
+
+func (s *Server) handlePolls(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		s.handlePollsGet(w, r)
+		return
+	case "POST":
+		s.handlePollsPost(w, r)
+		return
+	case "DELETE":
+		s.handlePollsDelete(w, r)
+		return
+	}
+
+	// not found
+	respondHTTPError(w, http.StatusNotFound)
+}
+
+func (s *Server) handlePollsGet(w http.ResponseWriter, r *http.Request) {
+	respondError(w, http.StatusInternalServerError, errors.New("not implemented"))
+}
+func (s *Server) handlePollsPost(w http.ResponseWriter, r *http.Request) {
+	respondError(w, http.StatusInternalServerError, errors.New("not implemented"))
+}
+func (s *Server) handlePollsDelete(w http.ResponseWriter,
+	r *http.Request) {
+	respondError(w, http.StatusInternalServerError, errors.New("not implemented"))
 }
