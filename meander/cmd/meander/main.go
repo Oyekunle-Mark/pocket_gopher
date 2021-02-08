d188c2d9657b0fa7190bf54810a0d73b1630e9a6
Oyekunle Oloyede <oyekunlemac@gmail.com>
2021-02-08 22:42:01 +0100

    Handle recommendation

--- meander/cmd/meander/main.go
+++ meander/cmd/meander/main.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"net/http"
 	"pocket_gopher/meander"
+	"strconv"
+	"strings"
 )
 
 func main() {
@@ -12,8 +14,42 @@ func main() {
 	http.HandleFunc("/journeys", func(w http.ResponseWriter, r *http.Request) {
 		respond(w, meander.Journeys)
 	})
+	http.HandleFunc("/recommendations", cors(func(w http.ResponseWriter, r *http.Request) {
+		q := &meander.Query{
+			Journey: strings.Split(r.URL.Query().Get("journey"), "|"),
+		}
+
+		var err error
+
+		q.Lat, err = strconv.ParseFloat(r.URL.Query().Get("lat"), 64)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		q.Lng, err = strconv.ParseFloat(r.URL.Query().Get("lng"), 64)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		q.Radius, err = strconv.Atoi(r.URL.Query().Get("radius"))
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		q.CostRangeStr = r.URL.Query().Get("cost")
+
+		places := q.Run()
+		respond(w, places)
+	}))
 
 	http.ListenAndServe(":8080", http.DefaultServeMux)
+}
+
+func cors(fn http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+	}
 }
 
 func respond(w http.ResponseWriter, data []interface{}) error {
