12ba6623a505e45c37aa171a710b1dfe5fb6b385
Oyekunle Oloyede <oyekunlemac@gmail.com>
2021-02-08 22:17:31 +0100

    ConstRange type with stringer and parser

--- meander/cost_level.go
+++ meander/cost_level.go
@@ -1,5 +1,10 @@
 package meander
 
+import (
+	"errors"
+	"strings"
+)
+
 type Cost int8
 
 const (
@@ -31,4 +36,27 @@ func (c Cost) String() string {
 
 func ParseCost(s string) Cost {
 	return costStrings[s]
+}
+
+type CostRange struct {
+	From Cost
+	To   Cost
+}
+
+func (c CostRange) String() string {
+	return c.From.String() + "..." + c.To.String()
+}
+
+func ParseCostRange(s string) (CostRange, error) {
+	var r CostRange
+	segs := strings.Split(s, "...")
+
+	if len(segs) != 2 {
+		return r, errors.New("invalid cost range")
+	}
+
+	r.From = ParseCost(segs[0])
+	r.To = ParseCost(segs[1])
+
+	return r, nil
 }
