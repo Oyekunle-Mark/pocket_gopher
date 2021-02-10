2a896e2ee627bd6419b14fb6d079af96ae08fb81
Oyekunle Oloyede <oyekunlemac@gmail.com>
2021-02-10 20:24:52 +0100

    loop

--- backup/cmds/backupd/main.go
+++ backup/cmds/backupd/main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
 	"flag"
+	"fmt"
 	"github.com/matryer/filedb"
 	"log"
+	"os"
+	"os/signal"
 	"pocket_gopher/backup"
+	"syscall"
 	"time"
 )
 
@@ -50,4 +56,44 @@ func main() {
 		fatalErr = err
 		return
 	}
+
+	var path path
+
+	col.ForEach(func(_ int, data []byte) bool {
+		if err := json.Unmarshal(data, &path); err != nil {
+			fatalErr = err
+			return true
+		}
+
+		m.Paths[path.Path] = path.Hash
+		return false // carry on
+	})
+
+	if fatalErr != nil {
+		return
+	}
+
+	if len(m.Paths) < 1 {
+		fatalErr = errors.New("no paths - use backup tool to add at least one")
+		return
+	}
+
+	for {
+		check(m, col)
+
+		signalChan := make(chan os.Signal, 1)
+		signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM)
+
+		for {
+			select {
+			case <-time.After(*interval):
+				check(m, col)
+			case <-signalChan: // stop
+				fmt.Println()
+				log.Printf("Stopping...")
+				return
+			}
+		}
+	}
 }
+
