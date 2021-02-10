ad599fff7d1118ec3526a7ed431fcd647c7e9304
Oyekunle Oloyede <oyekunlemac@gmail.com>
2021-02-10 20:21:02 +0100

    parse arguments

--- backup/cmds/backupd/main.go
+++ backup/cmds/backupd/main.go
@@ -1,1 +1,25 @@
 package main
+
+import (
+	"flag"
+	"log"
+	"time"
+)
+
+func main() {
+	var fatalErr error
+
+	defer func() {
+		if fatalErr != nil {
+			log.Fatalln(fatalErr)
+		}
+	}()
+
+	var (
+		interval = flag.Duration("interval", 10*time.Second, "interval between checks")
+		archive  = flag.String("archive", "archive", "path to archive location")
+		dbpath   = flag.String("db", "./db", "path to filedb database")
+	)
+
+	flag.Parse()
+}
