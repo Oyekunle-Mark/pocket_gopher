b27fad5934a70692d5bc501639482c2a420bd4f5
Oyekunle Oloyede <oyekunlemac@gmail.com>
2021-02-15 17:12:58 +0100

    The vault service interface

--- vault/service.go
+++ vault/service.go
@@ -7,3 +7,5 @@ type Service interface {
 	Hash(ctx context.Context, password string) (string, error)
 	Validate(ctx context.Context, password, hash string) (bool, error)
 }
+
+type vaultService struct{}
