6dffe214b4299accd603c6ffaef2355ff9310e33
Oyekunle Oloyede <oyekunlemac@gmail.com>
2021-02-15 17:39:24 +0100

    json request and response type

--- vault/service.go
+++ vault/service.go
@@ -37,3 +37,22 @@ func (vaultService) Validate(ctx context.Context, password, hash string) (bool, 
 
 	return true, nil
 }
+
+type hashRequest struct {
+	Password string `json:"password"`
+}
+
+type hashResponse struct {
+	Hash string `json:"hash"`
+	Err  string `json:"err,omitempty"`
+}
+
+type validateRequest struct {
+	Password string `json:"password"`
+	Hash     string `json:"hash"`
+}
+
+type validateResponse struct {
+	Valid bool   `json:"valid"`
+	Err   string `json:"err,omitempty"`
+}
